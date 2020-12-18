aaaf506a014d27c9a09d30d91946da68df23fa45
Jakub Sobka <57126560+Guandanarian@users.noreply.github.com>
2020-12-18 13:32:55 +0100

    Version upgrade

--- Ta_+1/Assets/Scripts/Test.cs
+++ Ta_+1/Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //elo
     }
 
     // Update is called once per frame
