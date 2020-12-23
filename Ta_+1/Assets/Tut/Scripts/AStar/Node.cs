7011248580c7bd79f2d070e0959ab7405255b548
Sebastian Tomczak <sebas.tomczak@gmail.com>
2020-12-23 12:09:22 +0100

    Part 6.1 Very first part of AStar algo

---
+++ Ta_+1/Assets/Tut/Scripts/AStar/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public Point GridPosition { get; private set; }
+
+    public TileScript TileScript { get; private set; }
+
+    public Node(TileScript tileScript)
+    {
+        GridPosition = tileScript.GridPosition;
+        TileScript = tileScript;
+    }
+}
