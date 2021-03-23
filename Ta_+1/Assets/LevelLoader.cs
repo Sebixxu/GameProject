6b8e798ae9306ee4e6da3d80e009c124a3b12c43
Sebastian Tomczak <sebas.tomczak@gmail.com>
2021-03-23 20:01:35 +0100

    Dodać główne Menu w oparciu o wstępne mockup'y #41 Dodać wstępny loading screen do projektu #42

---
+++ Ta_+1/Assets/LevelLoader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private Slider slider;
+
+    public void LoadLevel(string sceneName)
+    {
+        StartCoroutine(LoadAsynchronously(sceneName));
+    }
+
+    IEnumerator LoadAsynchronously(string sceneName)
+    {
+        loadingScreen.SetActive(true);
+
+        var operation = SceneManager.LoadSceneAsync(sceneName);
+        while (!operation.isDone)
+        {
+            var progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+
+            Debug.Log($"[LevelLoader] Loading scene: {sceneName}, progress: {progress}.");
+            yield return null;
+        }
+    }
+
+    private void PerformLoadLevel()
+    {
+    }
+}
---
+++ Ta_+1/Assets/MainMenuManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private LevelLoader levelLoader;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGameClick()
+    {
+        levelLoader.LoadLevel("Level0");
+    }
+
+    public void ContinueClick()
+    {
+        //TODO
+    }
+
+    public void HighScoreClick()
+    {
+        //TODO
+    }
+
+    public void HelpClick()
+    {
+        //TODO
+    }
+
+    public void SettingsClick()
+    {
+        //TODO
+    }
+
+    public void CreditsClick()
+    {
+        //TODO
+    }
+
+    public void ExitClick()
+    {
+        Application.Quit(0);
+    }
+}
