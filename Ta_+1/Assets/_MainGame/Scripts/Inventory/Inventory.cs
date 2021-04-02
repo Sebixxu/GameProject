7ebe6c88ae5034e8ac240a154a8c46021975c923
Sebastian Tomczak <sebas.tomczak@gmail.com>
2021-04-02 21:53:20 +0200

    Dodać panel z inventory oraz slotami ekwipunku gracza #53 Dodatkowo został dodany do projektu Universal RP - do shaderów oraz Smart Quick Search.

--- Ta_+1/Assets/_MainGame/Scripts/Inventory/Inventory.cs
+++ Ta_+1/Assets/_MainGame/Scripts/Inventory/Inventory.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Inventory
 {
-    private List<Item> items;
+    public event EventHandler OnItemListChanged;
 
-    public Inventory()
+    public List<Item> Items => items;
+    private List<Item> items;
+    private Action<Item> useItemAction;
+
+    public Inventory(Action<Item> useItemAction)
     {
+        this.useItemAction = useItemAction;
         items = new List<Item>();
 
+        AddItem(new Item { amount = 1, itemType = ItemType.Sword });
+        AddItem(new Item { amount = 2, itemType = ItemType.HealthPotion });
         AddItem(new Item { amount = 1, itemType = ItemType.Sword });
 
         Debug.Log(items.Count);
@@ -17,6 +26,52 @@ public class Inventory
 
     public void AddItem(Item item)
     {
-        items.Add(item);
+        if (item.IsStackable())
+        {
+            var currentItemInInventory = items.FirstOrDefault(x => x.itemType == item.itemType);
+
+            if (currentItemInInventory != null)
+            {
+                currentItemInInventory.amount += item.amount;
+            }
+            else
+            {
+                items.Add(item);
+            }
+        }
+        else
+        {
+            items.Add(item);
+        }
+
+        OnItemListChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void RemoveItem(Item item)
+    {
+        if (item.IsStackable())
+        {
+            var currentItemInInventory = items.FirstOrDefault(x => x.itemType == item.itemType);
+
+            if (currentItemInInventory != null && currentItemInInventory.amount > 1)
+            {
+                currentItemInInventory.amount -= 1;
+            }
+            else
+            {
+                items.Remove(item);
+            }
+        }
+        else
+        {
+            items.Remove(item);
+        }
+
+        OnItemListChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void UseItem(Item item)
+    {
+        useItemAction(item);
     }
 }
